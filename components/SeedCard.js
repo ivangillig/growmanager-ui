877375d75f761008e22b59c0c869abd551a07ee5
Ivan Gillig <ivan.gillig@gmail.com>
2025-03-10 22:28:19 -0300

    card adjusted

--- components/SeedCard.js
+++ components/SeedCard.js
@@ -1,25 +1,44 @@
 import React from 'react'
-import { Card } from 'antd'
+import { Card, Tooltip } from 'antd'
 import { getFullImageUrl } from '../utils/commonUtils'
 import { useTranslation } from 'next-i18next'
+import { FaEye, FaEdit, FaTrash } from 'react-icons/fa'
 
-const SeedCard = ({ seed }) => {
+const { Meta } = Card
+const SeedCard = ({ seed, onViewDetails, onEdit, onDelete }) => {
   const { t } = useTranslation()
   const imageUrl = seed.imageUrl
     ? getFullImageUrl(seed.imageUrl)
     : '/images/image_not_found.png'
 
   return (
-    <Card>
-      <img src={imageUrl} alt={seed.genetic} className="seed-card-image" />
-      <Card title={seed.genetic}>
-        <p>
-          {t('Seed Bank')}: {seed.seedBank}
-        </p>
-        <p>
-          {t('Chemo Type')}: {seed.chemoType}
-        </p>
-      </Card>
+    <Card
+      hoverable
+      cover={
+        <img src={imageUrl} alt={seed.genetic} className="seed-card-image" />
+      }
+      actions={[
+        <FaEye onClick={onViewDetails} title={t('View Details')} />,
+        <FaEdit onClick={onEdit} title={t('Edit')} />,
+        <FaTrash onClick={onDelete} title={t('Delete')} />,
+      ]}
+      className="seed-card"
+    >
+      <Meta
+        title={seed.genetic}
+        description={
+          <>
+            <Tooltip title={`${t('Seed Bank')}: ${seed.seedBank}`}>
+              <p className="seed-card-description">
+                {t('Seed Bank')}: {seed.seedBank}
+              </p>
+            </Tooltip>
+            <p className="seed-card-description">
+              {t('Chemo Type')}: {seed.chemoType}
+            </p>
+          </>
+        }
+      />
     </Card>
   )
 }

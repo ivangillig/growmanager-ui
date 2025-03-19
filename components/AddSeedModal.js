266081ef7f00eb6e651eb872d016f3afcd514a7a
Nahuelgg <nahuelgg90@gmail.com>
2025-03-19 16:54:06 -0300

    add_a_create_seed_bank_modal

--- components/AddSeedModal.js
+++ components/AddSeedModal.js
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import { Modal, Form, Input, Select, Upload, Button, Checkbox } from 'antd'
 import { useTranslation } from 'next-i18next'
 import { LuCloudUpload } from 'react-icons/lu'
+import { PlusOutlined } from '@ant-design/icons'
+import AddSeedBankModal from './AddSeedBankModal' // Importar el nuevo modal
 
 const { Option } = Select
 
@@ -10,6 +12,7 @@ const AddSeedModal = ({ visible, onCancel, onAddSeed }) => {
   const { t } = useTranslation()
   const [form] = Form.useForm()
   const [fileList, setFileList] = useState([])
+  const [isSeedBankModalVisible, setSeedBankModalVisible] = useState(false)
 
   const handleOk = () => {
     form.validateFields().then((values) => {
@@ -32,86 +35,101 @@ const AddSeedModal = ({ visible, onCancel, onAddSeed }) => {
   const handleUploadChange = ({ fileList }) => setFileList(fileList)
 
   return (
-    <Modal
-      open={visible}
-      title={t('Add New Seed')}
-      onCancel={onCancel}
-      onOk={handleOk}
-      okText={t('Add Strain')}
-      cancelText={t('Cancel')}
-    >
-      <Form form={form} layout="vertical">
-        <Form.Item
-          name="genetic"
-          label={t('Genetic name')}
-          rules={[{ required: true, message: t('Please input the genetic') }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="seedBank"
-          label={t('Seed Bank')}
-          rules={[{ required: true, message: t('Please input the seed bank') }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="chemoType"
-          label={t('Chemo Type')}
-          rules={[
-            { required: true, message: t('Please select the chemo type') },
-          ]}
-        >
-          <Select>
-            <Option value="1">1</Option>
-            <Option value="2">2</Option>
-            <Option value="3">3</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name="dominance"
-          label={t('Dominance')}
-          rules={[{ required: true, message: t('Please input the dominance') }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="ratio"
-          label={t('Ratio')}
-          rules={[{ required: true, message: t('Please input the ratio') }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="cannabinoids"
-          label={t('Cannabinoids')}
-          rules={[
-            {
-              required: true,
-              message: t('Please select at least one cannabinoid'),
-            },
-          ]}
-        >
-          <Checkbox.Group>
-            <Checkbox value="THC">THC</Checkbox>
-            <Checkbox value="CBD">CBD</Checkbox>
-            <Checkbox value="CBG">CBG</Checkbox>
-            <Checkbox value="CBN">CBN</Checkbox>
-            <Checkbox value="CBC">CBC</Checkbox>
-          </Checkbox.Group>
-        </Form.Item>
-        <Form.Item name="image" label={t('Image')}>
-          <Upload
-            listType="picture"
-            fileList={fileList}
-            onChange={handleUploadChange}
-            beforeUpload={() => false}
+    <>
+      <Modal
+        open={visible}
+        title={t('Add New Seed')}
+        onCancel={onCancel}
+        onOk={handleOk}
+        okText={t('Add Strain')}
+        cancelText={t('Cancel')}
+      >
+        <Form form={form} layout="vertical">
+          <Form.Item
+            name="genetic"
+            label={t('Genetic name')}
+            rules={[{ required: true, message: t('Please input the genetic') }]}
           >
-            <Button icon={<LuCloudUpload />}>{t('Upload')}</Button>
-          </Upload>
-        </Form.Item>
-      </Form>
-    </Modal>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="seedBank"
+            label={t('Seed Bank')}
+            rules={[{ required: true, message: t('Please select the seed bank') }]}
+          >
+            <div style={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
+              <Select style={{ flex: 1 }}>
+                <Option value="Bank1">{t('Bank1')}</Option>
+                <Option value="Bank2">{t('Bank2')}</Option>
+              </Select>
+              <Button
+                icon={<PlusOutlined />}
+                onClick={() => setSeedBankModalVisible(true)}
+              />
+            </div>
+          </Form.Item>
+          <Form.Item
+            name="chemoType"
+            label={t('Chemo Type')}
+            rules={[
+              { required: true, message: t('Please select the chemo type') },
+            ]}
+          >
+            <Select>
+              <Option value="1">1</Option>
+              <Option value="2">2</Option>
+              <Option value="3">3</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            name="dominance"
+            label={t('Dominance')}
+            rules={[{ required: true, message: t('Please input the dominance') }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="ratio"
+            label={t('Ratio')}
+            rules={[{ required: true, message: t('Please input the ratio') }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="cannabinoids"
+            label={t('Cannabinoids')}
+            rules={[
+              {
+                required: true,
+                message: t('Please select at least one cannabinoid'),
+              },
+            ]}
+          >
+            <Checkbox.Group>
+              <Checkbox value="THC">THC</Checkbox>
+              <Checkbox value="CBD">CBD</Checkbox>
+              <Checkbox value="CBG">CBG</Checkbox>
+              <Checkbox value="CBN">CBN</Checkbox>
+              <Checkbox value="CBC">CBC</Checkbox>
+            </Checkbox.Group>
+          </Form.Item>
+          <Form.Item name="image" label={t('Image')}>
+            <Upload
+              listType="picture"
+              fileList={fileList}
+              onChange={handleUploadChange}
+              beforeUpload={() => false}
+            >
+              <Button icon={<LuCloudUpload />}>{t('Upload')}</Button>
+            </Upload>
+          </Form.Item>
+        </Form>
+      </Modal>
+      <AddSeedBankModal
+        visible={isSeedBankModalVisible}
+        onCancel={() => setSeedBankModalVisible(false)}
+      />
+    </>
   )
 }
 
